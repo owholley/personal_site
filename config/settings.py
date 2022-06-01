9ff0016ce5e4f2c708a86d4c89bd0e53d7966b7b
Owen Wholley <owholley@gmail.com>
2022-05-31 18:45:43 -0700

    Made some alterations to get the project running in the vm

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('HOST'),
         'PORT': 5432
     }
 }
