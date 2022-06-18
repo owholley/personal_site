b20ec10b5531cd649006b766aa511b2d47ef5d43
Owen Wholley <owholley@gmail.com>
2022-06-17 21:43:02 -0400

    updated testing settings

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -10,4 +10,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['www.mainedogco.com', 'mainedogco.com']
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = True
